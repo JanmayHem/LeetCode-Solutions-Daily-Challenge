e7b7a9ecae933c3b1993b9d01289ce27cf8dc313
Janmay <77008411+JanmayHem@users.noreply.github.com>
2022-12-02 21:08:53 +0530

    Update and rename 279 - 22 Nov 2022 - Perfect Squares to 279 - 22 Nov 2022 - Perfect Squares.java

--- LeetCode-Solutions/279 - 22 Nov 2022 - Perfect Squares.java
+++ LeetCode-Solutions/279 - 22 Nov 2022 - Perfect Squares.java
@@ -7,16 +7,16 @@ class Solution {
     public int numSquares(int n) {
         int sqrt = (int) Math.sqrt(n);
 
-        if (sqrt * sqrt == n) // Perfect square
+        if (sqrt * sqrt == n)
             return 1;
 
-        while (n % 4 == 0) // 4^a (8b + 7)
+        while (n % 4 == 0)
             n = n / 4;
 
         if (n % 8 == 7)
             return 4;
 
-        for (int i = 1; i * i <= n; i++) { // Addition of two perfect squares
+        for (int i = 1; i * i <= n; i++) {
             int square = i * i;
             int base = (int) Math.sqrt(n - square);
             if (base * base == n - square)
