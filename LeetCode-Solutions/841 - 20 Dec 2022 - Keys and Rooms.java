7aeda3a336230688bdb11d1ef0961f61bb852ffb
Janmay <77008411+JanmayHem@users.noreply.github.com>
2022-12-20 20:34:10 +0530

    Create 841 - 20 Dec 2022 - Keys and Rooms.java

---
+++ LeetCode-Solutions/841 - 20 Dec 2022 - Keys and Rooms.java
@@ -0,0 +1,25 @@
+/*
+Runtime: 1 ms Beats 94.27%
+*/
+
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        if(rooms.size() == 0)
+            return true;
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0, rooms, visited);
+        for(int i=0; i<rooms.size(); i++){
+            if(!visited[i])
+                return false;
+        }
+        return true;
+    }
+    
+    public void dfs(int index, List<List<Integer>> rooms, boolean[] visited){
+        visited[index] = true;
+        for(int n: rooms.get(index)){
+            if(!visited[n])
+                dfs(n, rooms, visited);
+        }
+    }
+}
