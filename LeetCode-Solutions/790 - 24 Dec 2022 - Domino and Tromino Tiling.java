b25a8ceacfceef0d412ea727a80991121c77e9b0
Janmay <77008411+JanmayHem@users.noreply.github.com>
2022-12-24 20:48:32 +0530

    Create 790 - 24 Dec 2022 - Domino and Tromino Tiling.java

---
+++ LeetCode-Solutions/790 - 24 Dec 2022 - Domino and Tromino Tiling.java
@@ -0,0 +1,18 @@
+/*
+Runtime: 0 ms Beats 100%
+Memory: 39.1 MB Beats 96.38%
+*/
+
+class Solution {
+    public int numTilings(int n) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        long[] dp = new long[n+1];
+        dp[0]=1; dp[1]=1; dp[2]=2;
+        for(int i=3; i<=n; i++)
+            dp[i] = (dp[i-1]*2 + dp[i-3])%1000000007;
+        return (int)dp[n];
+    }
+}
