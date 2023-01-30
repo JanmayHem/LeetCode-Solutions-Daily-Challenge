5b6dafb77ab0a29f913d2427099bdf57b15e01b2
Janmay <77008411+JanmayHem@users.noreply.github.com>
2023-01-30 09:28:54 +0530

    Create 1137 - 30 Jan 2023 - N-th Tribonacci Number.java

---
+++ LeetCode-Solutions/1137 - 30 Jan 2023 - N-th Tribonacci Number.java
@@ -0,0 +1,19 @@
+/*
+Runtime: 0 ms Beats 100%
+*/
+
+class Solution {
+    public int tribonacci(int n) {
+        int t0=0, t1=1, t2=1, t=0;
+        if(n==0) return t0;
+        if(n==1) return t1;
+        if(n==2) return t2;
+        for(int i=2; i<n; i++){
+            t=t0+t1+t2;
+            t0=t1;
+            t1=t2;
+            t2=t;
+        }
+        return t;
+    }
+}
