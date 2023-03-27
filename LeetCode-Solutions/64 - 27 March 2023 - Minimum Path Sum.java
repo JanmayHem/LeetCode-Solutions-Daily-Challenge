00e3be5dc88a54d98d24a3b4e42979f9f8cef5da
Janmay <77008411+JanmayHem@users.noreply.github.com>
2023-03-27 13:34:00 +0530

    Create 64 - 27 March 2023 - Minimum Path Sum.java

---
+++ LeetCode-Solutions/64 - 27 March 2023 - Minimum Path Sum.java
@@ -0,0 +1,15 @@
+/*
+Runtime: 2 ms Beats 76.27%
+Memory: 45 MB Beats 71.59%
+*/
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        for(int i=1; i<grid.length; i++) grid[i][0] += grid[i-1][0];
+        for(int i=1; i<grid[0].length; i++) grid[0][i] += grid[0][i-1];
+        for(int i=1; i<grid.length; i++){
+            for(int j=1; j<grid[0].length; j++) grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+        }       
+        return grid[grid.length-1][grid[0].length-1];
+    }
+}
