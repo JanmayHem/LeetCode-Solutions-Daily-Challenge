e6f14865f078941abef6ae865cc60a97ff466fab
Janmay <77008411+JanmayHem@users.noreply.github.com>
2023-06-09 10:30:44 +0530

    Update 744 - 9 June 2023 - Find Smallest Letter Greater Than Target.java

--- LeetCode-Solutions/744 - 9 June 2023 - Find Smallest Letter Greater Than Target.java
+++ LeetCode-Solutions/744 - 9 June 2023 - Find Smallest Letter Greater Than Target.java
@@ -4,9 +4,7 @@ Runtime: 0 ms Beats 100%
 
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        for(char letter: letters){
-            if(letter>target) return letter;
-        }
+        for(char letter: letters) if(letter>target) return letter;
         return letters[0];
     }
 }
