13afd91a1c29f3abe1db29765a50a26df793d730
Janmay <77008411+JanmayHem@users.noreply.github.com>
2023-07-25 17:06:25 +0530

    Create 852 - 25 July 2023 - Peak Index in a Mountain Array.java

---
+++ LeetCode-Solutions/852 - 25 July 2023 - Peak Index in a Mountain Array.java
@@ -0,0 +1,23 @@
+/*
+Runtime: 0ms
+Beats 100.00% of users with Java
+
+Memory: 55.16mb
+Beats 80.80% of users with Java
+*/
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length, l=0, r=n-1;
+        return binarySearch(l, r, arr);
+    }
+
+    private int binarySearch(int l, int r, int[] arr){
+        int mid = (l+r)/2;
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
+        else{
+            if(arr[mid]<arr[mid+1]) return binarySearch(mid, r, arr);
+            else return binarySearch(l, mid, arr);
+        }
+    }
+}
