1725451a807d928ee42300225223da24ba27eb5d
Janmay <77008411+JanmayHem@users.noreply.github.com>
2023-08-07 00:30:33 +0530

    Create 920 - 7 Aug 2023 - Number of Music Playlists.java

---
+++ LeetCode-Solutions/920 - 7 Aug 2023 - Number of Music Playlists.java
@@ -0,0 +1,23 @@
+/*
+Runtime: 5 ms
+Beats 82.42%of users with Java
+
+Memory: 39.76 mb
+Beats 86.81%of users with Java
+*/
+
+class Solution {
+    public int numMusicPlaylists(int n, int goal, int k) {
+        int mod = (int)1e9+7;
+        long[][] dp = new long[2][n+1];
+        dp[0][0] = 1;
+        for(int i=1; i<=goal; i++){
+            dp[i%2][0] = 0;
+            for(int j=1; j<=Math.min(i,n); j++){
+                dp[i%2][j] = dp[(i+1)%2][j-1] * (n-(j-1)) % mod;
+                if(j>k) dp[i%2][j] = (dp[i%2][j] + dp[(i-1)%2][j] * (j-k)) % mod;
+            }
+        }
+        return (int)dp[goal%2][n];
+    }
+}
